d6a9350e5be524e17d1ec5f365735607d89ccec0
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2024-06-13 11:40:09 +0900

    work4実装

---
+++ work_4.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+import random
+
+# ↓↓↓ お約束のコード ↓↓↓
+window = tk.Tk()
+window.title("GUI App")
+window.geometry("600x400")
+bg_color = "#333333"  # ダークグレー
+fg_color = "#FFFFFF"  # 白
+window.configure(bg=bg_color)
+# ↑↑↑ お約束のコード ↑↑↑
+
+name_list = []
+
+
+def button_action():  # 関数の定義 ※ボタンが押されたときの動き
+    user_input = entry1.get()  # 入力値を取得
+    name_list.append(user_input)
+    label1.config(text="\n".join(name_list))  # 画面に出力
+
+
+def random_choice():  # 関数の定義 ※ボタンが押されたときの動き
+    index = random.randint(0, len(name_list) - 1)
+    label2.config(text=f"{name_list[index]}に決めた")  # 画面に出力
+
+
+# 入力フィールドの作成
+entry1 = tk.Entry(window, bg=fg_color, fg=bg_color)
+entry1.pack(pady=10)
+
+# ボタンの作成
+button1 = tk.Button(window, text="追加", command=button_action)
+button1.pack(pady=10)
+
+# 出力ラベルの作成
+label1 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+label1.pack(pady=10)
+
+# 名前の選択ボタン
+button2 = tk.Button(window, text="選ぶ", command=random_choice)
+button2.pack(pady=10)
+
+# ラベルの作成
+label2 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+label2.pack(pady=10)
+
+# ↓↓↓ お約束のコード ↓↓↓
+window.mainloop()
+# ↑↑↑ お約束のコード ↑↑↑
